e880f9fb0a3663871dd7d35d462fb651f097b1a2
Chris Campbell <christopherecampbell@gmail.com>
2023-04-17 17:36:55 -0600

    set up template for new components

---
+++ src/components/AAACompTemplate/CompTemplate.jsx
@@ -0,0 +1,25 @@
+// npm modules
+
+
+// page components
+
+
+// components
+
+
+// services
+
+
+// styles
+import styles from './CompTemplate.module.css'
+
+// component
+
+
+export default function CompTemplate() {
+  return (
+    <div className={styles.container}>
+      CompTemplate
+    </div>
+  )
+}
---
+++ src/components/HorizontalNavBar/HorizontalNavBar.jsx
@@ -0,0 +1,25 @@
+// npm modules
+
+
+// page components
+
+
+// components
+
+
+// services
+
+
+// styles
+import styles from './HorizontalNavBar.module.css'
+
+// component
+
+
+export default function HorizontalNavBar() {
+  return (
+    <div className={styles.container}>
+      HorizontalNavBar
+    </div>
+  )
+}
