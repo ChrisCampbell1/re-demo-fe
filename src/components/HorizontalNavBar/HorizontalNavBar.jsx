528128c8c574130618a0277097528353e0a182a2
Chris Campbell <christopherecampbell@gmail.com>
2023-04-25 16:51:59 -0600

    adjust nav links

--- src/components/HorizontalNavBar/HorizontalNavBar.jsx
+++ src/components/HorizontalNavBar/HorizontalNavBar.jsx
@@ -47,6 +47,7 @@ export default function HorizontalNavBar({ user, handleLogout }) {
           <li><NavLink to="/profiles">Profiles</NavLink></li>
           <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
           <li><NavLink to="/change-password">Change Password</NavLink></li>
+          <li><NavLink to="#">Admin</NavLink></li>
           <li className={styles.dropdown}><NavLink to="/change-password">Category 1</NavLink>
             <ul className={styles.dropdownContent}>
               <li><NavLink to='#'>Sub Cat 1</NavLink></li>
@@ -63,8 +64,19 @@ export default function HorizontalNavBar({ user, handleLogout }) {
         </ul>
       :
         <ul>
-          <li><NavLink to="/login">Log In</NavLink></li>
-          <li><NavLink to="/signup">Sign Up</NavLink></li>
+          <li className={styles.dropdown}><NavLink to="/change-password">Category 1</NavLink>
+            <ul className={styles.dropdownContent}>
+              <li><NavLink to='#'>Sub Cat 1</NavLink></li>
+              <li><NavLink to='#'>Sub Cat 2</NavLink></li>
+              <li className={styles.secondHover}><NavLink to='/change-password'>Sub Cat 2</NavLink>
+                <ul className={styles.secondDropdown}>
+                  <li><NavLink to='#'>Sub Cat 1</NavLink></li>
+                  <li><NavLink to='#'>Sub Cat 2</NavLink></li>
+                  <li><NavLink to='#'>Sub Cat 2</NavLink></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
         </ul>
       }
     </nav>
@@ -82,6 +94,7 @@ export default function HorizontalNavBar({ user, handleLogout }) {
             <li><NavLink to="/profiles">Profiles</NavLink></li>
             <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
             <li><NavLink to="/change-password">Change Password</NavLink></li>
+            <li><NavLink to="#">Admin</NavLink></li>
             <p onClick={handleSubClick}>Metro Areas</p>
               {displaySub &&
                 <>
@@ -100,8 +113,23 @@ export default function HorizontalNavBar({ user, handleLogout }) {
           </ul>
         :
           <ul>
-            <li><NavLink to="/login">Log In</NavLink></li>
-            <li><NavLink to="/signup">Sign Up</NavLink></li>
+            <li><NavLink to="#">Link 1</NavLink></li>
+            <li><NavLink to="#">Link 2</NavLink></li>
+            <p onClick={handleSubClick}>Metro Areas</p>
+              {displaySub &&
+                <>
+                  <li><NavLink to='#'>Sub Cat 1</NavLink></li>
+                  <li><NavLink to='#'>Sub Cat 2</NavLink></li>
+                  <p onClick={handleSubSubClick}>Denver</p>
+                    {displaySubSub &&
+                      <>
+                        <li><NavLink to='#'>Harvy Park</NavLink></li>
+                        <li><NavLink to='#'>Cap Hill</NavLink></li>
+                        <li><NavLink to='#'>Sunnyside</NavLink></li>
+                      </>
+                    }
+                </>
+              }
           </ul>
         }
       </div>
